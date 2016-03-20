fffdf7de49aa77f39609790b0e584e208f11a1c3
root <martija1@aston.ac.uk>
2016-03-20 10:11:33 -0400

    validate packages for meteor publish

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use("mizzao:user-status");
+  api.use("mizzao:user-status@0.6.6");
   api.use(['templating'], 'client');
   api.addFiles(['services/call.js']);
   api.addFiles(['template.html', 'template.js'], 'client');
