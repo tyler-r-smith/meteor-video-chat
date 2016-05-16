21435df0ed156620f8e5ba382b4e5d766d24fd56
Jack Martin <martija1@aston.ac.uk>
2016-05-16 13:45:34 +0000

    testdave

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'elmarti:video-chat',
-  version: '0.2.17',
+  version: '0.3.0',
   // Brief, one-line summary of the package.
   summary: 'Simple WebRTC Video Chat for your app.',
   // URL to the Git repository containing the source code for this package.
