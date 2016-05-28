9938abde3061ec4525b38ccc8c29224745eae069
Jack Martin <martija1@aston.ac.uk>
2016-05-28 04:57:14 +0000

    update readme, add webcam disposer

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'elmarti:video-chat',
-  version: '0.3.2',
+  version: '0.3.3',
   // Brief, one-line summary of the package.
   summary: 'Simple WebRTC Video Chat for your app.',
   // URL to the Git repository containing the source code for this package.
