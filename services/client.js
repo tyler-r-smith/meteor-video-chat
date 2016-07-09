c7e903766da9a339bba37d9419cf892e8ab49f3a
Jack Martin <martija1@aston.ac.uk>
2016-07-09 17:25:15 +0000

    rough firefox shim

--- services/client.js
+++ services/client.js
@@ -1,4 +1,18 @@
-
+navigator.sayswho= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M[0];
+})();
   
   
   
@@ -96,7 +110,7 @@
           Meteor.VideoCallServices.peerConnection.addStream(Meteor.localStream);
           return callback(stream);
         };
-        if (navigator.mediaDevices && $.browser.mozilla) {
+        if (navigator.mediaDevices && navigator.sayswho == "Firefox") {
           console.log("new type");
           navigator.mediaDevices.getUserMedia({
               video: true,
